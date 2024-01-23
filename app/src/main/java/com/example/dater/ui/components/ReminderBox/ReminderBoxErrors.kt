3a18bad57ba966e0b93d6b58be8db6e64d98dbdf
kanusukA <aayushkaushal74@gmail.com>
2024-01-23 17:52:36 +0530

    Added Reminder Completion Status
    Added Day count in Reminder Notification.
    
    Added Reminder Error Check.
    
    Fixed Reminder in JourneyBox, now shows no reminder text when none are.
    
    Fixed ReminderNotifierRequest, getReminderNotifierStatus, Now shows passed
    
    Fixed ReminderBox Cancel, now deletes reminder box when not previously saved in journey creation.
    
    Fixed Journey Delete, now deletes all the reminders associated with the journey.
    
    Fixed Create Journey view, now clears on each creation.
    
    Fixed Date Length in Reminder, now shows abbreviated date for current month and year.
    
     Removed JourneyBox Reminder Filter.

--- app/src/main/java/com/example/dater/ui/components/ReminderBox/ReminderBoxErrors.kt
+++ app/src/main/java/com/example/dater/ui/components/ReminderBox/ReminderBoxErrors.kt
@@ -1,13 +1,11 @@
 package com.example.dater.ui.components.ReminderBox
 
-data class ReminderBoxErrors (
-    val titleErrorLong: Boolean = false,
-    val titleErrorEmpty: Boolean = false,
-    val descriptionErrorLong: Boolean = false,
-    val startDateError: Boolean = false,
-    val endDateError: Boolean = false,
-    val dateErrorEmpty: Boolean = false
-)
+import android.content.Context
+import com.example.dater.Data.Reminder.domain.model.Reminder
+import com.example.dater.Data.Reminder.domain.model.ReminderDateType
+import com.example.dater.ToastMessage
+
+
 
 sealed class ReminderBoxErrorEvents {
     data class Title(val string: String): ReminderBoxErrorEvents()
@@ -15,4 +13,57 @@ sealed class ReminderBoxErrorEvents {
     data class StartDate(val long: Long): ReminderBoxErrorEvents()
     data class EndDate(val long: Long): ReminderBoxErrorEvents()
     object SaveCheck: ReminderBoxErrorEvents()
+}
+
+class ReminderErrorCheck(
+    private val context: Context,
+    private val reminder: Reminder,
+    val onUpdateReminder: (Reminder) -> Unit,
+    val onSaveReminder: (Reminder) -> Unit
+){
+    fun title(string: String){
+        if (string.length < 25){
+            onUpdateReminder(reminder.copy(title = string))
+        } else{
+            ToastMessage(context, " Title Long")
+        }
+    }
+
+    fun description(string: String){
+        if (string.length < 150){
+            onUpdateReminder(reminder.copy(description = string))
+        } else {
+            ToastMessage(context, "Description Long")
+        }
+    }
+
+    fun startDate(long: Long){
+        if (reminder.endDate > long || reminder.endDate == 0L){
+            onUpdateReminder(reminder.copy(startDate = long))
+        } else {
+            ToastMessage(context, "Invalid Date")
+        }
+    }
+
+    fun endDate(long: Long){
+        if (reminder.startDate < long || reminder.startDate == 0L){
+            onUpdateReminder(reminder.copy(endDate = long))
+        } else {
+            ToastMessage(context, "Invalid Date")
+        }
+    }
+
+    fun saveReminder(){
+        if (reminder.title.isBlank()){
+            ToastMessage(context,"Title empty")
+        } else if (reminder.dateType == ReminderDateType.SelectedDays && !reminder.selectedDays.contains(1)){
+            ToastMessage(context, "No Days Selected")
+        } else if (reminder.endDate == 0L && reminder.startDate == 0L && reminder.dateType != ReminderDateType.SelectedDays){
+            ToastMessage(context,"Date Empty")
+        } else {
+            onSaveReminder(reminder)
+        }
+
+    }
+
 }
